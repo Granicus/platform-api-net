9b10c02c7c377065c5c3cf5a4f233e6889e985f6
GRANICUSINC\Natalya.Varshavskaya <natalya.varshavskaya@granicus.com>
2025-04-07 19:23:37 -0500

    GUB-69227: modify Program class to launch StartUpSelectionForm

--- Meeting Simulator/Program.cs
+++ Meeting Simulator/Program.cs
@@ -14,7 +14,8 @@ namespace MeetingSimulator
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
+            Application.Run(new StartUpSelection());
         }
     }
 }
